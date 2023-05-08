f66fe25ce0ca17ceb95766a637ed9d5d48f12c4e
José Antonio Montero Martínez <81559171+itsjosean@users.noreply.github.com>
2023-05-08 12:31:43 +0200

    J2 - 08/05

--- Codigo/sopaDeLletres.c
+++ Codigo/sopaDeLletres.c
@@ -8,13 +8,13 @@
 
 /* Definicio del tipus per a desar les dades de la sopa de lletres */
 /* Es una proposta que podeu canviar, ampliar, etc. */
-typedef struct 
+typedef struct
 {
     char ll[MAX_LLETRES + 1];   // Lletres de la paraula (comptem centinella)
     bool enc;   // La paraula s'ha encertat
 } paraula_t;
 
-typedef struct 
+typedef struct
 {
     int dim;        // Nombre de files = nombre de columnes
     char *lletres;  // Taula amb les lletres
@@ -29,113 +29,189 @@ typedef struct
 /* que haureu de fer la vostra pròpia. */
 void genera_sopa(sopa_t *s)
 {
-    s->dim = 30;    // Mida màxima: 40 x 40
+
+    int midaSopa;
+    bool midaCorrecta;
+
+    while (!midaCorrecta)
+    {
+        printf("Introdueix la mida de la sopa de lletres:");
+        scanf("%d",&midaSopa);
+
+        if (midaSopa > 0 && midaSopa <= 40)
+        {
+            midaCorrecta = true;
+        }
+        else
+        {
+            printf("Aquesta mida no es valida. Torna a probar. (1 - 40)\n");
+        }
+    }
+
+    s->dim = midaSopa;    // Mida màxima: 40 x 40
     s->lletres = malloc(s->dim * s->dim * sizeof(char));   // Espai per a les lletres
     s->encertades = malloc(s->dim * s->dim * sizeof(char)); // Per saber si una lletra correspon a encert
+
     for (int i = 0; i < s->dim * s->dim; i++)
     {
-        s->encertades[i] = false;
         // Generem una lletra aleatoriament
         s->lletres[i] = 'A' + (rand() % ('Z'-'A' + 1));
     }
 
-    s->n_par = 5;
-    strcpy(s->par[0].ll, "ALZINA");
-    strcpy(s->par[1].ll, "ARBUST");
-    strcpy(s->par[2].ll, "BOLET");
-    strcpy(s->par[3].ll, "CAMI");
-    strcpy(s->par[4].ll, "PEDRA");
+    prepararSopaLletres(s);
 
-    s->n_encerts = 2;
-    s->par[0].enc = false;
-    s->par[1].enc = true;
-    s->par[2].enc = true;
-    s->par[3].enc = false;
-    s->par[4].enc = false;
-
-    // Ara posem un parell de paraules a la sopa com si s'haguessin encertat
-    s->lletres[5] = 'B'; s->encertades[5] = true;
-    s->lletres[6] = 'O'; s->encertades[6] = true;
-    s->lletres[7] = 'L'; s->encertades[7] = true;
-    s->lletres[8] = 'E'; s->encertades[8] = true;
-    s->lletres[9] = 'T'; s->encertades[9] = true;
-
-    s->lletres[65 + s->dim] = 'A'; s->encertades[65 + s->dim] = true;
-    s->lletres[65 + 2 * s->dim] = 'R'; s->encertades[65 + 2 * s->dim] = true;
-    s->lletres[65 + 3 * s->dim] = 'B'; s->encertades[65 + 3 * s->dim] = true;
-    s->lletres[65 + 4 * s->dim] = 'U'; s->encertades[65 + 4 * s->dim] = true;
-    s->lletres[65 + 5 * s->dim] = 'S'; s->encertades[65 + 5 * s->dim] = true;
-    s->lletres[65 + 6 * s->dim] = 'T'; s->encertades[65 + 6 * s->dim] = true;
-    
 }
-
 
 /* Mostra la sopa de lletres pel terminal */
 /* En principi, NO HAURIEU DE MODIFICAR AQUEST CODI SI NO TOQUEU LES ESTRUCTURES DE DADES*/
 void mostra_sopa (sopa_t *s)
 {
-    // Mostrem els numeros de columna
-    printf("\033[0;31m");   // Color 
-    printf("  ");
-    for (int i = 10; i < s->dim + 1; i+=10)
-    {
-        for (int j=0; j < 18; j++)
-            printf(" ");
-        printf(" %d", i/10);
-
-    }
-
-    printf("\n  ");   
-    for (int i = 0; i < s->dim; i++)
-    {   
-        int p = (i % 10) + 1;
-        p != 10 ? printf(" %d", p) : printf(" 0");
-    }
-
-    printf("\n");
-    printf("\033[0m");  // Tornem al color per defecte
-
-
-    // Mostrem les lletres. Cada lletra ocupa dos espais: 
-    // Si correspon a un encert, es marca
+    printf("\n~SOPA DE LLETRES~\n");
     for (int i = 0; i < s->dim ; i++)
     {
-        printf("\033[0;31m");   // Color 
-        printf("%-2d", i + 1);  // Mostrar numero de linia
-        printf("\033[0m");  // Tornem al color per defecte
-
         for (int j = 0; j < s->dim; j++)
         {
-            if (s->encertades[i * s->dim + j]) 
-            {
-                printf("\033[0;42m");   // Color verd de fons
-                printf(" %c", s->lletres[i * s->dim + j]);
-                printf("\033[0m");  // Tornem al color per defecte
-            }
-            else
-            {
-                printf(" %c", s->lletres[i * s->dim + j]);
-            }
+            printf("|%c|", s->lletres[i * s->dim + j]);
+
         }
         printf("\n");
     }
+}
+
+void guardarParaulesArxius (sopa_t *s, char paraules[], char nomArxiu[])
+{
+
+    FILE *arxiu;
+    arxiu = fopen(nomArxiu, "r");
+
+    char paraula[100];
+
+    if (arxiu == NULL)
+    {
+        printf("Error al intentar obrir el arxiu.");
+    }
+    else
+    {
+        printf("\nLlista de paraules a trobar: \n");
+        while(!feof(arxiu))
+        {
+            fgets(paraula, 100, arxiu);
+            printf("%s", paraula);
+        }
+    }
+
     printf("\n");
 
-    printf("Portes %d encerts.\n", s->n_encerts);
-    printf("Paraules a trobar: %d\n", s->n_par - s->n_encerts);
-    for (int i = 0; i < s->n_par; i++)
+    fclose(arxiu);
+}
+
+void jugarPartida (sopa_t *sopa)
+{
+    bool *partidaEnJoc = true;
+    char eleccioJugador[10];
+    int resultatRendicio, resultatEleccio, filaUsuari, columnaUsuari, direccioUsuari;
+
+    mostra_sopa(sopa);
+
+    while (&partidaEnJoc)
     {
-        if (!s->par[i].enc)
-            printf("%s\n", s->par[i].ll);
+        printf("\n's' has trobat una paraula  - 'RENDICIO' per veure la solucio\n");
+        scanf("%s", &eleccioJugador);
+
+        resultatEleccio = strcmp(eleccioJugador, "s");
+
+        if (resultatEleccio == 0)
+        {
+            printf("PARAULA TROBADA...?");
+
+            // DEMANAR FILA Y COLUMNA
+            printf("\nIntrodueix les 'coordenades' de la paraula trobada:");
+
+            printf("\nFila: ");
+            scanf("%s", &filaUsuari);
+
+            printf("\nColumna: ");
+            scanf("%s", &columnaUsuari);
+
+            printf("\nDireccio: ");
+            scanf("%s", &direccioUsuari);
+
+            // COMPROVAR LA PARAULA
+            comprovacioParaula(&sopa, filaUsuari, columnaUsuari, direccioUsuari, &jugarPartida);
+
+            printf("SOPA:\n");
+            mostra_sopa(sopa);
+
+        }
+        else
+        {
+            resultatRendicio = strcmp(eleccioJugador, "RENDICIO");
+
+            if (resultatRendicio == 0)
+            {
+                // MOSTRAR SOLUCIO
+                printf("MOSTRAR SOLUCIO");
+                partidaEnJoc = false;
+            }
+            else
+            {
+                // PARAULA INCORRECTA
+                printf("Valor incorrecte. Torna a provar.");
+            }
+        }
     }
+}
+
+void comprovacioParaula (sopa_t *sopa, int filaUsuari, int columnaUsuari, int direccioUsuari, bool *estatPartida)
+{
+    printf("COMPROVANT PARAULA...");
+
+
+
+    // *estatPartida = false;
+}
+
+
+void prepararSopaLletres (sopa_t *sopa)
+{
+
+    //int posicioFilaInical = rand() % sopa->dim;
+    //int posicioColumnaInical = rand() % sopa->dim;
+
+    char paraula[] = "JOSE";
+
+    sopa->lletres[2] = 'J';
+
+
 
 }
 
-int main() {
+void missatgeBenvinguda ()
+{
+
+    printf("******************************************************\n");
+    printf("Us donem la benvinguda al joc de la sopa de lletres!\n");
+    printf("Autors: Pablo, Jose, Edgar\n");
+    printf("******************************************************\n");
+}
+
+int main()
+{
+    char paraules[100];
+    char nomArxiu[100];
+
+    missatgeBenvinguda();
 
     sopa_t sopa;    // La sopa de lletres
 
+    printf("Nom arxiu: ");
+    scanf("%s", &nomArxiu);
+
+    guardarParaulesArxius(&sopa, paraules, nomArxiu);
+
     genera_sopa(&sopa);     // La generem (exemple)
+
+    jugarPartida(&sopa);
 
     return 0;
 }
